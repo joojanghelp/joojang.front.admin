196fe1a8f589696d10c7c48f08c1e54753208af7
sm.park <sm.park@healthmax.co.kr>
2020-04-03 13:49:40 +0900

    Updates

---
+++ precache-manifest.bf55cfcaedecaf051b1974c066861b5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e4648e4ea62ef701fcf686b8fbb7cbf",
+    "url": "/joojang.front.admin/index.html"
+  },
+  {
+    "revision": "28f1bcc44321dd838682",
+    "url": "/joojang.front.admin/static/css/main.f527be85.chunk.css"
+  },
+  {
+    "revision": "adba165f238367c42422",
+    "url": "/joojang.front.admin/static/js/2.cfcbf53f.chunk.js"
+  },
+  {
+    "revision": "28f1bcc44321dd838682",
+    "url": "/joojang.front.admin/static/js/main.84ef760a.chunk.js"
+  },
+  {
+    "revision": "1ed591e5423df7c6e0de",
+    "url": "/joojang.front.admin/static/js/runtime-main.68f396b1.js"
+  }
+]);

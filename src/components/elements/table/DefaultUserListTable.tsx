2f27ed6ed298315963c87b9f9ed897b249048e49
sm.park <sm.park@healthmax.co.kr>
2020-04-13 13:44:25 +0900

    버튼 사이즈 조정.

--- src/components/elements/table/DefaultUserListTable.tsx
+++ src/components/elements/table/DefaultUserListTable.tsx
@@ -31,20 +31,6 @@ function DefaultUserListTable({ isloading, items, handleUserInfoLink, handleUser
                         <th>탈퇴</th>
                     </tr>
                 </thead>
-                <tfoot>
-                    <tr>
-                        <th>이메일</th>
-                        <th>이름</th>
-                        <th>가입형태</th>
-                        <th>상태</th>
-                        <th>등급</th>
-                        <th>올린책</th>
-                        <th>활동수</th>
-                        <th>가입일</th>
-                        <th>중지</th>
-                        <th>탈퇴</th>
-                    </tr>
-                </tfoot>
                 {isloading === 'loading'
                     ?
                     <tbody><tr><td colSpan={10}><div style={{ height: '10vh', display: 'flex', justifyContent: 'center', alignItems: 'center',}}><LoadingSpinner /></div></td></tr></tbody>
@@ -65,13 +51,13 @@ function DefaultUserListTable({ isloading, items, handleUserInfoLink, handleUser
                                 (function(){
                                     switch(item.active) {
                                         case 'Y' :
-                                            return <td><button type="button" className="btn btn-primary" onClick={() => handleUserActiveUpdateLink(item.uuid, 'N')}>중지</button></td>
+                                            return <td><button type="button" className="btn btn-primary btn-sm" onClick={() => handleUserActiveUpdateLink(item.uuid, 'N')}>중지</button></td>
                                         case 'N' :
-                                            return <td><button type="button" className="btn btn-danger" onClick={() => handleUserActiveUpdateLink(item.uuid, 'Y')}>활성</button></td>
+                                            return <td><button type="button" className="btn btn-danger btn-sm" onClick={() => handleUserActiveUpdateLink(item.uuid, 'Y')}>활성</button></td>
                                     }
                                 })()
                             }
-                            <td><button type="button" className="btn btn-danger" onClick={() => handleUserActiveDeleteLink(item.uuid)}>탈퇴</button></td>
+                            <td><button type="button" className="btn btn-danger btn-sm" onClick={() => handleUserActiveDeleteLink(item.uuid)}>탈퇴</button></td>
                         </tr>
                     )
                     }
